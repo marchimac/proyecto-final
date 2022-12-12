d4d2f12fc08bc2c932287207f36a1868faadf570
Fernando <fernandoms1078@hotmail.com>
2022-12-12 21:35:44 +0100

    Ahora si termino la clase

--- src/__tests__/components/NavbarComp.test.js
+++ src/__tests__/components/NavbarComp.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import NavbarComp from "../../components/NavbarComp";
 
-test('Localiza una clase', () => {
+test('Localizar una clase', () => {
     render(<NavbarComp />)
     const tested = screen.getByTestId('tested')
     expect(tested).toHaveClass('me-auto')
